159c97f0e9eb1f5c591536a4c4a1a97f2f510158
Giang Hoang <gianghoang.9955@gmail.com>
2023-05-06 21:01:41 +0700

    update

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        // productionではhttpsを強制する
+        if (\App::environment('production')) {
+            \URL::forceScheme('https');
+        } else if (\App::environment('local')) {
+            \URL::forceScheme('https');
+        }
+    }
+}
